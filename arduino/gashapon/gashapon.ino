a414c7040c58444def6567828338bbd088db0d98
Alexander Fix <afix@alexanderfix.com>
2026-05-09 22:51:53 -0500

    Now working TFT and coin slot

--- arduino/gashapon/gashapon.ino
+++ arduino/gashapon/gashapon.ino
@@ -1,5 +1,5 @@
 
-#define USE_TFT_DISPLAY 0
+#define USE_TFT_DISPLAY 1
 
 
 #if USE_TFT_DISPLAY
@@ -13,6 +13,11 @@
 #define NEOPIXEL_PIN 5
 #define POWER_PIN    10
 
+
+// Proximity sensor
+#include <Adafruit_VCNL4030.h>
+
+Adafruit_VCNL4030 vcnl;
 
 
 #if USE_TFT_DISPLAY
@@ -39,6 +44,16 @@ void setup() {
   Serial.begin(115200);
   Serial.println("GASHapon machine starting up...");
 
+  if (!vcnl.begin()) {
+    Serial.println(F("Could not find a valid VCNL4030 sensor, check wiring!"));
+    while (1) {
+      delay(10);
+    }
+  }
+
+  Serial.println(F("VCNL4030 Found!"));
+  Serial.println();
+
 // TFT Setup
 #if USE_TFT_DISPLAY
   // For the Feather ESP32-S2, we need to enable I2C power first!
@@ -51,15 +66,45 @@ void setup() {
   pinMode(PIN_I2C_POWER, OUTPUT);
   digitalWrite(PIN_I2C_POWER, !polarity);
 #endif // defined(ARDUINO_ADAFRUIT_FEATHER_ESP32S2)
+
+  // Initialize display
+  // turn on backlite
+  pinMode(TFT_BACKLITE, OUTPUT);
+  digitalWrite(TFT_BACKLITE, HIGH);
+
+  // turn on the TFT / I2C power supply
+  pinMode(TFT_I2C_POWER, OUTPUT);
+  digitalWrite(TFT_I2C_POWER, HIGH);
+  delay(10);
+
+  // initialize TFT
+  tft.init(135, 240); // Init ST7789 240x135
+  tft.setRotation(3);
+  tft.fillScreen(ST77XX_BLACK);
+
+  tft.setTextColor(ST77XX_WHITE);
+  tft.setTextWrap(false);
+
+  display_test(0);
 #endif // USE_TFT_DISPLAY
 }
 
 int i = 0;
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(i++);
-  delay(100);
+  // Serial.println(i++);
+  // delay(100);
 #if USE_TFT_DISPLAY
-  display_test(i);
+  // display_test(i);
 #endif // USE_TFT_DISPLAY
+
+  uint16_t prox = vcnl.readProximity();
+  Serial.println(prox);
+  if (prox > 1200) {
+    Serial.print("Detected!");
+    Serial.println(i++);
+    display_test(1);
+    delay(500);
+    display_test(0);
+  }
 }
