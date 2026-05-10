c6a73d1dcf51ccdcf3aa62c8945c5c3f06824986
Alexander Fix <afix@alexanderfix.com>
2026-05-10 04:15:12 -0500

    Working LEDs

--- arduino/gashapon/gashapon.ino
+++ arduino/gashapon/gashapon.ino
@@ -1,6 +1,7 @@
 
 #define USE_TFT_DISPLAY 1
 #define USE_PROX_SENSOR 1
+#define USE_LEDS 1
 
 
 #if USE_TFT_DISPLAY
@@ -14,6 +15,59 @@
 #define NEOPIXEL_PIN 5
 #define POWER_PIN    10
 
+
+// create a neopixel strip
+static const int num_pixels_total = 133;
+static const int start_pixel = 13;
+static const int num_pixels = num_pixels_total - start_pixel;
+uint32_t color_array[num_pixels];
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(num_pixels_total, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
+
+void blank_pattern(uint32_t* color_array) {
+  for (int i = 0; i < num_pixels; ++i) {
+    color_array[i] = 0;
+  }
+}
+
+void test_ruler(uint32_t* color_array) {
+  for (int i = 0; i < num_pixels; ++i) {
+    if (i % 10 == 0) {
+      color_array[i] = strip.Color(255, 0, 0);
+    }
+    else {
+      color_array[i] = 0;
+    }
+  }
+}
+
+void show_poland(bool polarity, int width) {
+  int stripe_counter = 0;
+  bool do_red = true;
+  for (int i = 0; i < num_pixels; ++i) {
+    if (stripe_counter > width) {
+      do_red = !do_red;
+      stripe_counter = 0;
+    }
+    stripe_counter++;
+
+    if (do_red) {
+      color_array[i] = strip.Color(255, 0, 0);
+    } else {
+      color_array[i] = strip.Color(255, 255, 255);
+    }
+  }
+}
+
+void show_pattern(uint32_t* color_array) {
+  for (int i = 0; i < start_pixel; ++i) {
+    strip.setPixelColor(i, 0);
+  }
+  for (int i = 0; i < num_pixels; ++i) {
+    strip.setPixelColor(i+start_pixel, color_array[i]);
+  }
+  digitalWrite(POWER_PIN, HIGH);
+  strip.show();
+}
 
 #if USE_PROX_SENSOR
 // Proximity sensor
@@ -46,12 +100,14 @@ void setup() {
   Serial.begin(115200);
   Serial.println("GASHapon machine starting up...");
 
+#if USE_PROX_SENSOR
   if (!vcnl.begin()) {
     Serial.println(F("Could not find a valid VCNL4030 sensor, check wiring!"));
     while (1) {
       delay(10);
     }
   }
+#endif // USE_PROX_SENSOR
 
   Serial.println(F("VCNL4030 Found!"));
   Serial.println();
@@ -89,6 +145,16 @@ void setup() {
 
   display_test(0);
 #endif // USE_TFT_DISPLAY
+
+
+  // Neo-pixel setup
+  // Set power pin to output
+  pinMode(POWER_PIN, OUTPUT);
+  // Disable the pin, we're not currently writing to the neopixels.
+  digitalWrite(POWER_PIN, LOW);
+
+  // This initializes the NeoPixel library.
+  strip.begin();
 }
 
 int i = 0;
@@ -113,4 +179,10 @@ void loop() {
     delay(500);
     display_test(0);
   }
+
+  // test_ruler(color_array);
+  show_poland(true, 7);
+  show_pattern(color_array);
+  delay(1000);
+
 }
